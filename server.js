c37bb96b4d7a6d7b03f7e10cd7bdc6fcb4dc6d10
unknown <jahrough-lopez@CNU4429H47.na.odcorp.net>
2016-01-11 19:46:09 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,30 +2,19 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
-MongoClient.connect("mongodb://" + process.env.IP + ":27017/kjmg", function(err, db) {
+MongoClient.connect("mongodb://" + process.env.IP + ":27017/kjmg", function (err, db) {
 	if (!err) {
 
-		var http = require('http');
-		const PORT = process.env.PORT;
-
-var express = require("express");
-var app     = express();
-var path    = require("path");
+		var path = require("path"),
+			express = require("express"),
+			app = express();
 
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-	
-
-		app.listen(PORT, function() {
-			console.log('Example app listening on port 3000!');
+		app.get('/', function (req, res) {
+			res.sendFile(path.join(__dirname + '/index.html'));
 		});
 
-
-		console.log("We are connected MongDB");
-
+		app.listen(process.env.PORT);
 
 	}
 
