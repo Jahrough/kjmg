e383bcd1146872129e49e50ff1ce7d670f89105a
jlopez <jahlopez81@gmail.com>
2016-01-18 23:26:12 +0000

    update functionality

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ MongoClient.connect('mongodb://' + process.env.IP + ':27017/kjmg', function(err,
 					});
 				}
 			});
-		//	db.close();
+			//	db.close();
 
 
 
@@ -35,14 +35,14 @@ MongoClient.connect('mongodb://' + process.env.IP + ':27017/kjmg', function(err,
 		});
 
 		app.post('/delete', function(req, res) {
-		
+
 			db.open(function(err, db) {
-				
+
 				if (!err) {
 
-					db.collection('complexContact').remove({_id: new mongoDB.ObjectID(req.body.id)}, function(err, data) {
-						console.log("error" +err);
-						console.log("Data" +data);
+					db.collection('complexContact').remove({
+						_id: new mongoDB.ObjectID(req.body.id)
+					}, function(err, data) {
 						db.collection('complexContact').find().toArray(function(err, data) {
 							if (!err) {
 								res.send(data);
@@ -53,7 +53,7 @@ MongoClient.connect('mongodb://' + process.env.IP + ':27017/kjmg', function(err,
 
 				}
 			});
-		
+
 
 
 
@@ -63,16 +63,23 @@ MongoClient.connect('mongodb://' + process.env.IP + ':27017/kjmg', function(err,
 		app.post('/update', function(req, res) {
 			db.open(function(err, db) {
 				if (!err) {
-					db.collection('complexContact').insert(req.body);
-
-					db.collection('complexContact').find().toArray(function(err, data) {
-						if (!err) {
-							res.send(data);
-						}
+					db.collection('complexContact').update({
+						_id: new mongoDB.ObjectID(req.body.id)
+					}, {
+						name: req.body.name,
+						phone: req.body.phone
+					}, function(err, data) {
+						db.collection('complexContact').find().toArray(function(err, data) {
+							if (!err) {
+								res.send(data);
+							}
+						});
 					});
+
+
 				}
 			});
-		
+
 		});
 		app.listen(process.env.PORT);
 
